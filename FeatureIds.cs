599c1d1bc3379a37744c1663992702b773ed6044
Krisztián Németh <krisztian.nemeth@lombiq.com>
2021-03-30 15:05:28 +0200

    Typo fix

--- FeatureIds.cs
+++ FeatureIds.cs
@@ -2,7 +2,7 @@ namespace Lombiq.UIKit
 {
     public static class FeatureIds
     {
-        private const string FeatureIdPrefix = "Lombiq.UiKit.";
+        private const string FeatureIdPrefix = "Lombiq.UIKit.";
 
         public const string ShowCase = FeatureIdPrefix + nameof(ShowCase);
     }
