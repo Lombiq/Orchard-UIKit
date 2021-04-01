17c6d87d6fccb72cdbbf190f0c405811c0224822
Krisztián Németh <krisztian.nemeth@lombiq.com>
2021-04-01 20:08:44 +0200

    Moving js to another file

---
+++ Assets/Scripts/dropDownEditor.js
@@ -0,0 +1,40 @@
+function removeSelectedFromDropDown(buttonId) {
+    /* Remove selected */
+
+    const button = $('#' + buttonId + '.dropDown__Button');
+    const buttonItems = button.siblings().children();
+    const buttonText = button[0].innerText.trim();
+
+    $.each(buttonItems, function arrangeClasses(index, item) {
+        if (buttonText === item.innerText.trim()) {
+            item.classList.add('d-none');
+        }
+        else {
+            item.classList.remove('d-none');
+        }
+    });
+}
+
+/* eslint-disable no-unused-vars */
+/* It's used from the dropDown shape */
+function dropdownOnClick(id, buttonId, aspId, removeSelected) {
+    return $('#' + id + ' div a').on('click', function dropDownOnClickFunc() {
+        const state = $(this).data('state');
+        const text = $(this).data('state-text');
+        const btnClass = $(this).data('class');
+        $('#' + buttonId)
+            .text(text)
+            .removeClass('btn-warning btn-success btn-danger')
+            .addClass(btnClass);
+
+        /* Set hidden input value */
+        $('#' + aspId).val(state);
+
+        if (removeSelected) {
+            removeSelectedFromDropDown(buttonId);
+        }
+    });
+}
+/* eslint-enable no-unused-vars */
+
+
