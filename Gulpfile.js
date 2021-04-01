97f2fc8c67870f6edc193d4059c92e025227727c
Krisztián Németh <krisztian.nemeth@lombiq.com>
2021-04-01 20:02:17 +0200

    Updating gulp files to be able to generate styles and javascripts also

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,4 +1,16 @@
 const gulp = require('gulp');
+const watch = require('gulp-watch');
+const paths = require('./Gulp/paths');
+const scssTargets = require('../../Utilities/Lombiq.Gulp.Extensions/Tasks/scss-targets');
 const jsTargets = require('../../Utilities/Lombiq.Gulp.Extensions/Tasks/js-targets');
 
-gulp.task('default', () => jsTargets.compile('./Assets/Scripts/', './wwwroot/lombiq-uikitscripts'));
+
+gulp.task('build:styles', scssTargets.build(paths.styles.base, paths.dist.css));
+
+gulp.task('clean', scssTargets.clean(paths.dist.css));
+
+gulp.task('build:js', () => jsTargets.compile(paths.scripts.base, paths.dist.js));
+
+gulp.task('watch:styles', () => watch(paths.styles.all, { verbose: true }, gulp.parallel('build:styles')));
+
+gulp.task('default', gulp.parallel('build:styles', 'build:js'));
