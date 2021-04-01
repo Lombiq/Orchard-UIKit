5185bab18ff9d22e806c0e6248cdc8bb52ff6ef2
Krisztián Németh <krisztian.nemeth@lombiq.com>
2021-04-01 20:35:55 +0200

    Instead of hiding placeholder on click, now it will be hidden on typing

--- Assets/Scripts/textBoxEditor.js
+++ Assets/Scripts/textBoxEditor.js
@@ -5,15 +5,15 @@
             $(this).siblings('input').focus();
         });
 
-        $('.form-control').focus(function formControlFocus() {
-            $(this).siblings('.textBoxEditor__placeholder').hide();
-        });
-
         $('.form-control').blur(function formControlBlur() {
             const $this = $(this);
             if ($this.val().length === 0) {
                 $(this).siblings('').show();
             }
+        });
+
+        $('.textBoxEditor__input.required').on('input', function onInput() {
+            $(this).siblings('.textBoxEditor__placeholder').hide();
         });
 
         $('.form-control').blur();
