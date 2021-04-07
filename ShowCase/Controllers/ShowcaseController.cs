e9241a26ea2708ca5a8b4fbaab83e1b36ba892ce
Krisztián Németh <krisztian.nemeth@lombiq.com>
2021-04-07 11:37:51 +0200

    Show ShowCase page on the admin dashboard.

--- ShowCase/Controllers/ShowcaseController.cs
+++ ShowCase/Controllers/ShowcaseController.cs
@@ -1,11 +1,13 @@
 using Lombiq.UIKit.ShowCase.Constants;
 using Lombiq.UIKit.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using OrchardCore.Admin;
 using OrchardCore.Modules;
 
 namespace Lombiq.UIKit.ShowCase.Controllers
 {
     [Feature(FeatureIds.ShowCase)]
+    [Admin]
     public class ShowCaseController : Controller
     {
         [HttpGet(Routes.UIKitShowCase)]
