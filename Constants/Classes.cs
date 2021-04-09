57013c92a54cb5d114aeb249a2ab0c9f2654f2d3
Krisztián Németh <krisztian.nemeth@lombiq.com>
2021-04-09 12:43:26 +0200

    Adding default classes

---
+++ Constants/Classes.cs
@@ -0,0 +1,16 @@
+namespace Lombiq.UIKit.Constants
+{
+    public static class Classes
+    {
+        public const string DefaultCheckBoxBlockName = "checkBoxEditor";
+
+        public const string InputElement = "__input";
+        public const string LabelElement = "__label";
+        public const string AsteriskElement = "__asterisk";
+
+        public const string LabelPositionModifierLeft = "_left";
+        public const string LabelPositionModifierRight = "_right";
+
+        public const string RequiredModifier = "_required";
+    }
+}
