456f5d66bbd2b63348db5f965693419a390a9b3d
Krisztián Németh <krisztian.nemeth@lombiq.com>
2021-04-09 13:35:03 +0200

    DropDown cleanup

--- Constants/Classes.cs
+++ Constants/Classes.cs
@@ -3,10 +3,15 @@ namespace Lombiq.UIKit.Constants
     public static class Classes
     {
         public const string DefaultCheckBoxBlockName = "checkBoxEditor";
+        public const string DefaultDropDownBlockName = "dropDownEditor";
 
         public const string InputElement = "__input";
         public const string LabelElement = "__label";
+        public const string ButtonElement = "__button";
         public const string AsteriskElement = "__asterisk";
+        public const string ButtonContainerElement = "__ButtonContainer";
+        public const string ItemContainerElement = "__itemContainer";
+        public const string ItemElement = "__item";
 
         public const string LabelPositionModifierLeft = "_left";
         public const string LabelPositionModifierRight = "_right";
