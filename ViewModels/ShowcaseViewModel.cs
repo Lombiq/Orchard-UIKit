7b1e5b39d993cf420a053346bf37a0299d01dd41
Krisztián Németh <krisztian.nemeth@lombiq.com>
2021-04-09 13:35:19 +0200

    Adding disabled inputs

--- ViewModels/ShowcaseViewModel.cs
+++ ViewModels/ShowcaseViewModel.cs
@@ -14,24 +14,31 @@ namespace Lombiq.UIKit.ViewModels
         [Required]
         public string TextBox1 { get; set; }
         public string TextBox2 { get; set; }
+        public string TextBox2Disabled { get; set; }
 
         [Required]
         public string TextBox3 { get; set; }
         public string TextBox4 { get; set; }
+        public string TextBox4Disabled { get; set; }
 
         [Required]
         public string TextBox5 { get; set; }
         public string TextBox6 { get; set; }
+        public string TextBox6Disabled { get; set; }
 
         [Required]
         public bool CheckboxFalse1 { get; set; }
         public bool CheckboxFalse2 { get; set; }
+        public bool CheckboxFalse2Disabled { get; set; }
 
         [Required]
         public bool CheckboxTrue1 { get; set; } = true;
         public bool CheckboxTrue2 { get; set; } = true;
+        public bool CheckboxTrue2Disabled { get; set; } = true;
 
         public bool DropDown { get; set; }
+        public bool DropDownDisabled { get; set; }
         public Status Status { get; set; }
+        public Status StatusDisabled { get; set; }
     }
 }
