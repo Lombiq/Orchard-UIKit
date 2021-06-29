9a0d3d62ac30f9cc42ae6c07c51de2594e3991bb
Krisztián Németh <krisztian.nemeth@lombiq.com>
2021-06-29 17:14:18 +0200

    Small changes to fulfill new project requirements

--- Manifest.cs
+++ Manifest.cs
@@ -2,15 +2,16 @@ using OrchardCore.Modules.Manifest;
 using static Lombiq.UIKit.FeatureIds;
 
 [assembly: Module(
-    Name = "Lombiq UiKit",
+    Name = "Lombiq UIKit",
     Author = "Lombiq Technologies",
     Version = "1.0",
-    Description = "Module for reusable shapes containing text editors, dropdown editors, and potentially more complex editors."
+    Description = "Module for reusable shapes containing text editors, dropdown editors, and potentially more complex editors.",
+    Website = "https://github.com/Lombiq/Orchard-UIKit"
 )]
 
 [assembly: Feature(
     Id = ShowCase,
-    Name = "Lombiq UiKit - Showcase page",
+    Name = "Lombiq UIKit - Showcase page",
     Category = "Development",
     Description = "You can reach the showcase page at /UIKitShowCase"
 )]
