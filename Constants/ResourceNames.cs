27b3e2fb04ee7b36174b140bc8e33532ae1aeae4
Krisztián Németh <krisztian.nemeth@lombiq.com>
2021-06-30 17:30:25 +0200

    Resource linking changes

--- Constants/ResourceNames.cs
+++ Constants/ResourceNames.cs
@@ -3,8 +3,10 @@ namespace Lombiq.UIKit.Constants
     public static class ResourceNames
     {
         public const string LombiqDropdownScript = nameof(LombiqDropdownScript);
+        public const string LombiqDropdownItemScript = nameof(LombiqDropdownItemScript);
         public const string LombiqDropdownStyle = nameof(LombiqCheckBoxStyle);
         public const string LombiqTextBoxStyle = nameof(LombiqTextBoxStyle);
+        public const string LombiqTextBoxScript = nameof(LombiqTextBoxScript);
         public const string LombiqCheckBoxStyle = nameof(LombiqCheckBoxStyle);
     }
 }
