30259a61de2dc6b2e025ce8f0335404985ae4c48
Márk Lukovics <mark.lukovics@lombiq.com>
2022-07-13 11:21:03 +0200

    Figuring out pathing

--- Lombiq.UIKit.Tests.UI/Extensions/TestCaseUITestContextExtensions.cs
+++ Lombiq.UIKit.Tests.UI/Extensions/TestCaseUITestContextExtensions.cs
@@ -1,23 +1,19 @@
-﻿using Atata;
-using Lombiq.Tests.UI.Extensions;
-using Lombiq.Tests.UI.Services;
-using OpenQA.Selenium;
-using Shouldly;
-using System;
-using System.Threading.Tasks;
-
-namespace Lombiq.UIKit.Tests.UI.Extensions;
-
-public static class TestCaseUITestContextExtensions
-{
-    public static async Task TestUIKitShowcaseBehaviorAsync(this UITestContext context)
-    {
-        await context.TestGroupCountAsync();
-        await context.TestGroupCountAsync();
-    }
-
-    public static async Task TestGroupCountAsync(this UITestContext context)
-    {
-        throw new NotImplementedException();
-    }
-}
+﻿// using Lombiq.Tests.UI.Services;
+// using System;
+// using System.Threading.Tasks;
+//
+// namespace Lombiq.UIKit.Tests.UI.Extensions;
+//
+// public static class TestCaseUITestContextExtensions
+// {
+//     public static async Task TestUIKitShowcaseBehaviorAsync(this UITestContext context)
+//     {
+//         await context.TestGroupCountAsync();
+//         await context.TestGroupCountAsync();
+//     }
+//
+//     public static async Task TestGroupCountAsync(this UITestContext context)
+//     {
+//         throw new NotImplementedException();
+//     }
+// }
