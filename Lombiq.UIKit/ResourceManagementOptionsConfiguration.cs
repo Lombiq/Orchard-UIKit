617c8ce2aea4ee9dae56a2a00c16cab37fa8f306
Márk Lukovics <mark.lukovics@lombiq.com>
2022-07-14 18:08:47 +0200

    Updating script and style helpers

---
+++ Lombiq.UIKit/ResourceManagementOptionsConfiguration.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.Options;
+using OrchardCore.ResourceManagement;
+using static Lombiq.UIKit.Constants.ResourceNames;
+
+namespace Lombiq.UIKit;
+
+public class ResourceManagementOptionsConfiguration : IConfigureOptions<ResourceManagementOptions>
+{
+    private const string Module = "~/Lombiq.UIKit/";
+    private const string Css = Module + "css/";
+    private const string Js = Module + "js/";
+
+    private static readonly ResourceManifest _manifest = new();
+
+    public ResourceManagementOptionsConfiguration()
+    {
+        _manifest
+            .DefineScript(LombiqDropdownScript)
+            .SetUrl(Js + "dropdown-editor.js");
+
+        _manifest
+            .DefineScript(LombiqTextBoxScript)
+            .SetUrl(Js + "textbox-editor.js");
+
+        _manifest
+            .DefineStyle(LombiqCheckBoxStyle)
+            .SetUrl(Css + "checkbox.min.css", Css + "checkbox.css");
+
+        _manifest
+            .DefineStyle(LombiqDropdownStyle)
+            .SetUrl(Css + "dropdown.min.css", Css + "dropdown.css");
+
+        _manifest
+            .DefineStyle(LombiqShowcaseStyle)
+            .SetUrl(Css + "showcase.min.css", Css + "showcase.css");
+
+        _manifest
+            .DefineStyle(LombiqTextBoxStyle)
+            .SetUrl(Css + "textbox.min.css", Css + "textbox.css");
+    }
+
+    public void Configure(ResourceManagementOptions options) => options.ResourceManifests.Add(_manifest);
+}
