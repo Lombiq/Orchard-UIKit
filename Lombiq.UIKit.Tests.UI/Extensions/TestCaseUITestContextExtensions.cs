568620c7d327bb49e40fdc544cd495da545122c9
Márk Lukovics <mark.lukovics@lombiq.com>
2022-07-14 11:41:09 +0200

    Refining tests

--- Lombiq.UIKit.Tests.UI/Extensions/TestCaseUITestContextExtensions.cs
+++ Lombiq.UIKit.Tests.UI/Extensions/TestCaseUITestContextExtensions.cs
@@ -12,53 +12,19 @@ public static class TestCaseUITestContextExtensions
     public static async Task TestUIKitShowcaseBehaviorAsync(this UITestContext context)
     {
         await context.SignInDirectlyAndGoToRelativeUrlAsync("UIKitShowcase");
-        context.TestGroupCount();
         await context.TestAccordionAsync();
         await context.TestDropdownButtonAsync();
     }
 
-    public static void TestGroupCount(this UITestContext context)
-    {
-        // Groups are correct
-        string[] correctGroupNames =
-        {
-            "Textbox:",
-            "Checkbox:",
-            "Custom classes:",
-            "Hint positioning:",
-            "Bootstrap dropdown:",
-            "Bootstrap controls:",
-        };
-
-        var groupNames = context.GetAll(By.CssSelector("showcaseContainer__item h1"));
-
-        groupNames.ShouldBeSameAs(correctGroupNames);
-    }
-
     public static async Task TestAccordionAsync(this UITestContext context)
     {
+        await context.ClickReliablyOnAsync(By.Id("heading-first"));
+
         var accordionFirst = context.Get(By.Id("collapse-first"));
+        accordionFirst.HasClass("show").ShouldBeTrue();
 
-        await accordionFirst.ClickReliablyAsync(context);
-
-        accordionFirst.HasClass("show");
-
-        var closedIcons = context.GetAll(By.CssSelector(".bootstrapAccordionSample .card-header-icon"));
-        var openIcons = context.GetAll(By.CssSelector(".bootstrapAccordionSample .card-header-icon .hide"));
-
-        for (var i = 0; i < closedIcons.Count; i++)
-        {
-            if (i == 0)
-            {
-                closedIcons[i].Displayed.ShouldBeFalse();
-                openIcons[i].Displayed.ShouldBeTrue();
-            }
-            else
-            {
-                closedIcons[i].Displayed.ShouldBeTrue();
-                openIcons[i].Displayed.ShouldBeFalse();
-            }
-        }
+        var openIcons = context.GetAll(By.CssSelector(".bootstrapAccordionSample .card-header-icon.hide"));
+        openIcons.Count.ShouldBe(1);
     }
 
     public static async Task TestDropdownButtonAsync(this UITestContext context)
