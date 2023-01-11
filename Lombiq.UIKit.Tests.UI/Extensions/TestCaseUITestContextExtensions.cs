a882ee04e5a59ec0a88141535055bf3b99a540f6
Oliver Friedrich <oliver.friedrich@lombiq.com>
2023-01-11 13:57:42 +0100

    Revert "Temporarily added code to investigate the stacktrace"
    This reverts commit 954e736613ece587f5d04bceba20b37b75eb5e4d.

--- Lombiq.UIKit.Tests.UI/Extensions/TestCaseUITestContextExtensions.cs
+++ Lombiq.UIKit.Tests.UI/Extensions/TestCaseUITestContextExtensions.cs
@@ -1,4 +1,4 @@
-using Atata;
+﻿using Atata;
 using Lombiq.Tests.UI.Extensions;
 using Lombiq.Tests.UI.Services;
 using OpenQA.Selenium;
@@ -26,9 +26,6 @@ public static class TestCaseUITestContextExtensions
 
         var openIcons = context.GetAll(By.CssSelector(".bootstrapAccordionSample .card-header-icon.hide"));
         openIcons.Count.ShouldBe(1);
-
-        // Added temporarily to investigate the stacktrace of this extension method.
-        context.AssertVisualVerificationApproved(element: null, pixelErrorPercentageThreshold: 5, regionOfInterest: null);
     }
 
     public static async Task TestDropdownButtonAsync(this UITestContext context)
