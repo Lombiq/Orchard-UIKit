62ef54c3d7ad7d675275e06f790c1f69049c3dd7
Roland Márkus <roland.markus@lombiq.com>
2024-06-10 11:15:46 +0200

    Fixing controller.

--- Lombiq.UIKit/Showcase/Controllers/ShowcaseController.cs
+++ Lombiq.UIKit/Showcase/Controllers/ShowcaseController.cs
@@ -8,9 +8,9 @@ namespace Lombiq.UIKit.Showcase.Controllers;
 
 [Feature(FeatureIds.Showcase)]
 [Admin]
-[Route("[controller]")]
+[Route(Routes.UIKitShowcase)]
 public class ShowcaseController : Controller
 {
-    [HttpGet(Routes.UIKitShowcase)]
+    [HttpGet]
     public ActionResult Showcase() => View(model: new ShowcaseViewModel());
 }
