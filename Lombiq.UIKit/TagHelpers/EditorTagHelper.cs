c1a1b5a1bf5e1a5a4efd7e70172b4ade58cc2f18
Márk Bartha <barthamark@gmail.com>
2024-10-31 17:18:35 +0100

    Adding max length option to textboxes

--- Lombiq.UIKit/TagHelpers/EditorTagHelper.cs
+++ Lombiq.UIKit/TagHelpers/EditorTagHelper.cs
@@ -65,6 +65,9 @@ public class EditorTagHelper : BaseShapeTagHelper
     [HtmlAttributeName(nameof(InputClasses))]
     public string InputClasses { get; set; }
 
+    [HtmlAttributeName(nameof(MaxLength))]
+    public int MaxLength { get; set; }
+
     // Otherwise the taghelper can't bind its value to it.
 #pragma warning disable CA2227 // Collection properties should be read only
 #pragma warning disable MA0016 // Prefer return collection abstraction instead of implementation
