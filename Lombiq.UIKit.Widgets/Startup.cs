a8df717b31e7945890e936b114c49f1c1b6d29e7
Miloš Jovanović <milosjovanovic042@gmail.com>
2024-12-05 00:06:52 +0100

    Removing Controller, Views and cleaning up Startup.cs for Widgets module.

--- Lombiq.UIKit.Widgets/Startup.cs
+++ Lombiq.UIKit.Widgets/Startup.cs
@@ -1,18 +1,6 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Routing;
-using Microsoft.Extensions.DependencyInjection;
 using OrchardCore.Modules;
 
-namespace Lombiq.UIKit.Widgets
+namespace Lombiq.UIKit.Widgets;
+public sealed class Startup : StartupBase
 {
-    public sealed class Startup : StartupBase
-    {
-        public override void ConfigureServices(IServiceCollection services)
-        {
-        }
-
-        public override void Configure(IApplicationBuilder builder, IEndpointRouteBuilder routes, IServiceProvider serviceProvider)
-        {
-        }
-    }
 }
