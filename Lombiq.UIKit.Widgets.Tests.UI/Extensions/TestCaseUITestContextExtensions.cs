36a942d3475700a31fe3679449ae8b863ee4f4dc
Miloš Jovanović <milosjovanovic042@gmail.com>
2024-12-22 19:28:58 +0100

    Removing async methods from Lombiq.UIKit.Widgets UI Test case class.

--- Lombiq.UIKit.Widgets.Tests.UI/Extensions/TestCaseUITestContextExtensions.cs
+++ Lombiq.UIKit.Widgets.Tests.UI/Extensions/TestCaseUITestContextExtensions.cs
@@ -1,26 +1,24 @@
 using Atata;
-using Lombiq.Tests.UI.Constants;
-using Lombiq.Tests.UI.Exceptions;
 using Lombiq.Tests.UI.Extensions;
 using Lombiq.Tests.UI.Services;
 using OpenQA.Selenium;
 using Shouldly;
 using System;
-using System.Text.Json;
-using System.Threading.Tasks;
 
 namespace Lombiq.UIKit.Widgets.Tests.UI.Extensions;
 
 public static class TestCaseUITestContextExtensions
 {
-    public static async Task TestUIKitWidgetsBehaviorAsync(this UITestContext context)
+    public static void TestUIKitWidgetsBehavior(this UITestContext context)
     {
         context.TestCarouselWidgetContainerExistence();
         context.TestSlickCarouselExistence();
         context.TestCorrectNumberOfItemsIsDisplayed();
     }
+
     public static void TestCorrectNumberOfItemsIsDisplayed(this UITestContext context, int numberOfItems = 1) =>
         context.GetAll(By.ClassName("slick-active")).Count.ShouldBe(numberOfItems);
+
     public static void TestSlickCarouselExistence(this UITestContext context) =>
         context.Exists(By.ClassName("slickCarousel__carousel"));
 
