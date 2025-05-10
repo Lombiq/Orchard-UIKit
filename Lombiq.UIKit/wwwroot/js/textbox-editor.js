d01017d6c44ba509a02abb74647cd819679317b5
Sára El-Saig <sara.el-saig@lombiq.com>
2025-05-10 18:14:04 +0200

    Fix other eslint warnings.

--- Lombiq.UIKit/wwwroot/js/textbox-editor.js
+++ Lombiq.UIKit/wwwroot/js/textbox-editor.js
@@ -1,13 +1,13 @@
-document.querySelectorAll('.textboxEditor__placeholder').forEach(item =>
-    item.addEventListener('click', () =>
-        item.parentElement.querySelector('.textboxEditor__input').focus()));
+document.querySelectorAll('.textboxEditor__placeholder').forEach((item) => item
+    .addEventListener('click', () => item
+        .parentElement.querySelector('.textboxEditor__input').focus()));
 
 function hidePlaceholder(item) {
     const placeholder = item.parentElement.querySelector('.textboxEditor__placeholder');
     if (placeholder) placeholder.hidden = item.value?.toString().trim();
 }
 
-document.querySelectorAll('.textboxEditor__input').forEach(item => {
+document.querySelectorAll('.textboxEditor__input').forEach((item) => {
     item.addEventListener('blur', () => hidePlaceholder(item));
     hidePlaceholder(item);
 
