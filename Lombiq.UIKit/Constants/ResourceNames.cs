50d3e01d410626a7aa1e7bdeea10fe28ee0b1cb7
Sára El-Saig <sara.el-saig@lombiq.com>
2025-05-11 23:24:47 +0200

    Fix broken resource names.

--- Lombiq.UIKit/Constants/ResourceNames.cs
+++ Lombiq.UIKit/Constants/ResourceNames.cs
@@ -3,8 +3,7 @@ namespace Lombiq.UIKit.Constants;
 public static class ResourceNames
 {
     public const string LombiqDropdownScript = nameof(LombiqDropdownScript);
-    public const string LombiqDropdownItemScript = nameof(LombiqDropdownItemScript);
-    public const string LombiqDropdownStyle = nameof(LombiqCheckBoxStyle);
+    public const string LombiqDropdownStyle = nameof(LombiqDropdownStyle);
     public const string LombiqTextBoxStyle = nameof(LombiqTextBoxStyle);
     public const string LombiqTextBoxScript = nameof(LombiqTextBoxScript);
     public const string LombiqCheckBoxStyle = nameof(LombiqCheckBoxStyle);
