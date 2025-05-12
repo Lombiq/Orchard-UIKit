9adfc0bda59a71fd5c698c18fe3353a0e246615a
Sára El-Saig <sara.el-saig@lombiq.com>
2025-05-12 15:14:52 +0200

    Revert.

--- Lombiq.UIKit/Constants/ResourceNames.cs
+++ Lombiq.UIKit/Constants/ResourceNames.cs
@@ -3,6 +3,7 @@ namespace Lombiq.UIKit.Constants;
 public static class ResourceNames
 {
     public const string LombiqDropdownScript = nameof(LombiqDropdownScript);
+    public const string LombiqDropdownItemScript = nameof(LombiqDropdownItemScript);
     public const string LombiqDropdownStyle = nameof(LombiqDropdownStyle);
     public const string LombiqTextBoxStyle = nameof(LombiqTextBoxStyle);
     public const string LombiqTextBoxScript = nameof(LombiqTextBoxScript);
