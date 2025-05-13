31f4f4934b8eeb8b54c99b2596a96af7ec350447
Sára El-Saig <sara.el-saig@lombiq.com>
2025-05-13 23:33:28 +0200

    Rewrite editors to use Bootstrap.

--- Lombiq.UIKit/wwwroot/js/textbox-editor.js
+++ Lombiq.UIKit/wwwroot/js/textbox-editor.js
@@ -1,17 +1,13 @@
-document.querySelectorAll('.textboxEditor__placeholder').forEach((item) => item
-    .addEventListener('click', () => item
-        .parentElement.querySelector('.textboxEditor__input').focus()));
-
-function hidePlaceholder(item) {
-    const placeholder = item.parentElement.querySelector('.textboxEditor__placeholder');
-    if (placeholder) placeholder.hidden = item.value?.toString().trim();
-}
-
 document.querySelectorAll('.textboxEditor__input').forEach((item) => {
-    item.addEventListener('blur', () => hidePlaceholder(item));
-    hidePlaceholder(item);
-
-    if (item.classList.contains('textboxEditor__input_required')) {
-        item.addEventListener('input', () => hidePlaceholder(item));
+    function hidePlaceholder(item, placeholder) {
+        placeholder.hidden = item.value?.toString().trim();
     }
+
+    const placeholder = item.parentElement.querySelector('.textboxEditor__placeholder');
+    if (!placeholder) return;
+
+    ['blur', 'keyup', 'change'].forEach((eventName) => item
+        .addEventListener(eventName, () => hidePlaceholder(item, placeholder)));
+
+    hidePlaceholder(item, placeholder);
 });
