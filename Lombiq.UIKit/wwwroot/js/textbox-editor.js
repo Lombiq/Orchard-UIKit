cd2dfd6c35893027ecd9653dbeb94fb667acc568
Sára El-Saig <sara.el-saig@lombiq.com>
2025-05-13 23:49:57 +0200

    Fix missing semicolon.

--- Lombiq.UIKit/wwwroot/js/textbox-editor.js
+++ Lombiq.UIKit/wwwroot/js/textbox-editor.js
@@ -1,6 +1,6 @@
 // Focus on the textbox after clicking on the icon on the left.
 document.querySelectorAll('.input-group-prepend:has(.textboxEditor__iconContainer)').forEach((item) => item
-    .addEventListener('click', () => item.parentElement.querySelector('.textboxEditor__input').focus()))
+    .addEventListener('click', () => item.parentElement.querySelector('.textboxEditor__input').focus()));
 
 // Hide custom placeholder when there is text in the textbox.
 document.querySelectorAll('.textboxEditor__input').forEach((item) => {
