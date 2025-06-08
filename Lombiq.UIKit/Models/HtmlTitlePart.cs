de4bcdfa65b697b951a0b35149ec2ff4decc1bc0
Sára El-Saig <sara.el-saig@lombiq.com>
2025-06-08 19:29:25 +0200

    Move HtmlTitlePart logic into model.

--- Lombiq.UIKit/Models/HtmlTitlePart.cs
+++ Lombiq.UIKit/Models/HtmlTitlePart.cs
@@ -1,6 +1,12 @@
+using AngleSharp;
+using AngleSharp.Dom;
 using Lombiq.HelpfulLibraries.Common.Utilities;
 using OrchardCore.ContentFields.Fields;
 using OrchardCore.ContentManagement;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Lombiq.UIKit.Models;
 
@@ -8,5 +14,31 @@ public sealed class HtmlTitlePart : ContentPart
 {
     public HtmlField Title { get; set; } = new();
 
-    public string GetPlainText() => HtmlHelper.ConvertToPlainText(Title.Html);
+    public static async Task<string> CleanUpHtmlAsync(string html)
+    {
+        var node = HtmlHelper.ParseHtmlFragment(html);
+        var elements = node
+            .Descendants<IElement>()
+            .Where(element =>
+                element.ParentElement != null &&
+                element.TagName.ToUpperInvariant() is "DIV" or "P")
+            .ToList();
+
+        foreach (var element in elements)
+        {
+            var parent = element.ParentElement!;
+
+            foreach (var child in element.ChildNodes.ToList())
+            {
+                parent.InsertBefore(child, element);
+            }
+
+            parent.RemoveChild(element);
+        }
+
+        var builder = new StringBuilder();
+        await using (var writer = new StringWriter(builder)) { await node.ToHtmlAsync(writer); }
+
+        return builder.ToString();
+    }
 }
