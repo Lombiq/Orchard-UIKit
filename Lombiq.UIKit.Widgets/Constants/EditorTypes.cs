c8f24cfe7ae49553f10586792091dd217a0cc241
Sára El-Saig <sara.el-saig@lombiq.com>
2025-06-13 18:45:45 +0200

    Add ImageLinkWidget templates.

---
+++ Lombiq.UIKit.Widgets/Constants/EditorTypes.cs
@@ -0,0 +1,9 @@
+namespace Lombiq.UIKit.Widgets.Constants;
+
+public static class EditorTypes
+{
+    public static class TextField
+    {
+        public const string ImageLinkWidgetLinks = nameof(ImageLinkWidgetLinks);
+    }
+}
