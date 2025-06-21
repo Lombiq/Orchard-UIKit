7408b963138bf938820a6041f5e5a3c1beb04ff1
Sára El-Saig <sara.el-saig@lombiq.com>
2025-06-21 18:16:28 +0200

    Set the default value of the DisplayType field.

--- Lombiq.UIKit.Widgets/Migrations/AddMetadataWidgetMigrations.cs
+++ Lombiq.UIKit.Widgets/Migrations/AddMetadataWidgetMigrations.cs
@@ -1,21 +1,25 @@
 using Lombiq.HelpfulLibraries.OrchardCore.Contents;
 using Lombiq.UIKit.Widgets.Models;
+using OrchardCore.ContentFields.Settings;
 using OrchardCore.ContentManagement.Metadata;
 using OrchardCore.ContentManagement.Metadata.Builders;
 using OrchardCore.Flows.Models;
 
 namespace Lombiq.UIKit.Widgets.Migrations;
 
-public class AddMetadataMigrations : WidgetMigrationBase<AddMetadataWidget>
+public class AddMetadataWidgetMigrations : WidgetMigrationBase<AddMetadataWidget>
 {
-    public AddMetadataMigrations(IContentDefinitionManager contentDefinitionManager)
+    public AddMetadataWidgetMigrations(IContentDefinitionManager contentDefinitionManager)
         : base(contentDefinitionManager)
     { }
 
     protected override void DefinePart(ContentPartDefinitionBuilder<AddMetadataWidget> builder) => builder
         .WithField(part => part.Alternate)
         .WithField(part => part.Classes, field => field.WithEditor(ContentFieldEditorEnums.TextFieldEditors.TextArea))
-        .WithField(part => part.DisplayType)
+        .WithField(part => part.DisplayType, field => field.WithSettings(new TextFieldSettings
+        {
+            DefaultValue = CommonContentDisplayTypes.Detail,
+        }))
         .WithField(part => part.Wrapper);
 
     protected async override Task<int> AdditionalCreateAsync()
--- Lombiq.UIKit.Widgets/Startup.cs
+++ Lombiq.UIKit.Widgets/Startup.cs
@@ -42,6 +42,6 @@ public sealed class Startup : StartupBase
             .UseDisplayDriver<RandomWidgetDisplayDriver>();
 
         services.AddContentPart<AddMetadataWidget>()
-            .WithMigration<AddMetadataMigrations>();
+            .WithMigration<AddMetadataWidgetMigrations>();
     }
 }
