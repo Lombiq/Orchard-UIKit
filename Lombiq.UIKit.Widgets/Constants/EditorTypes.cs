bc4411b1c4860d589f0abdb689c2ffbfb004c91d
Sára El-Saig <sara.el-saig@lombiq.com>
2025-06-22 18:40:29 +0200

    Not needed.

--- Lombiq.UIKit.Widgets/Constants/EditorTypes.cs
+++
@@ -1,9 +0,0 @@
-namespace Lombiq.UIKit.Widgets.Constants;
-
-public static class EditorTypes
-{
-    public static class TextField
-    {
-        public const string ImageLinkWidgetLinks = nameof(ImageLinkWidgetLinks);
-    }
-}
