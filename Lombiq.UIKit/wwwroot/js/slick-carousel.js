2b68feccf0af8c4af070303e2519a2b821928968
Sára El-Saig <sara.el-saig@lombiq.com>
2025-06-23 12:09:41 +0200

    Re-add the init hack.

--- Lombiq.UIKit/wwwroot/js/slick-carousel.js
+++ Lombiq.UIKit/wwwroot/js/slick-carousel.js
@@ -30,6 +30,12 @@ jQuery(($) => {
             carouselSettings.slide = '';
         }
 
-        $(carousel).slick(carouselSettings);
+        $(carousel)
+            .on("init", () => {
+                // Carousel initializes elements with empty id attributes, which triggers HTML validation errors.
+                // See: https://github.com/kenwheeler/slick/issues/4296
+                carousel.querySelectorAll('[id=""]').forEach((element) => element.removeAttribute('id'));
+            })
+            .slick(carouselSettings);
     });
 });
