6eecd37a64ceb352e73d675ebd440a50471c2c36
Sára El-Saig <sara.el-saig@lombiq.com>
2025-06-23 12:01:26 +0200

    refactor code to be closer to vanilla.

--- Lombiq.UIKit/wwwroot/js/slick-carousel.js
+++ Lombiq.UIKit/wwwroot/js/slick-carousel.js
@@ -1,41 +1,35 @@
 jQuery(($) => {
-    document.querySelectorAll('.slickCarousel').forEach((element) => {
-        const $carouselContainer = $(element);
-        const $carousel = $carouselContainer.find('.slickCarousel__carousel');
-        const $carouselArrows = $carouselContainer.find('.slickCarousel__arrows');
+    document.querySelectorAll('.slickCarousel').forEach((containerElement) => {
+        const carousel = containerElement.querySelector('.slickCarousel__carousel');
 
-        $carousel.on('init', () => {
-            // Carousel initializes elements with empty id attributes, which triggers HTML validation errors.
-            // See: https://github.com/kenwheeler/slick/issues/4296
-            $('.slickCarousel__item[id=""]').removeAttr('id');
-        });
+        const findItems = () => Array.from(carousel.querySelectorAll('.slickCarousel__item'));
 
         let carouselSettings = {
             autoplay: true,
             mobileFirst: true,
-            appendArrows: $carouselArrows,
+            appendArrows: '.slickCarousel__arrows',
         };
 
-        const mergeSettingsJson = element.getAttribute('data-merge-settings');
+        const mergeSettingsJson = containerElement.getAttribute('data-merge-settings');
         if (mergeSettingsJson && mergeSettingsJson[0] === '{') {
             const mergeSettings = JSON.parse(mergeSettingsJson);
             carouselSettings = $.extend(true, carouselSettings, mergeSettings);
         }
 
-        // Apply the slide selector destructively, to avoid problems from the slide elements being on different
-        // levels.
+        // Apply the slide selector destructively, to avoid problems from the slide elements being on different levels.
         if (carouselSettings.slide?.trim()) {
-            const $items = $carousel.find('.slickCarousel__item');
-            const $slides = $items.find(carouselSettings.slide);
+            const originalItems = findItems();
+            const $slides = $(carouselSettings.slide, originalItems)
+                .addClass('slickCarousel__item')
+                .appendTo(carousel);
 
             if ($slides.length) {
-                $carousel.append($slides.addClass('slickCarousel__item'));
-                $items.remove();
+                originalItems.forEach((item) => item.remove());
             }
 
             carouselSettings.slide = '';
         }
 
-        $carousel.slick(carouselSettings);
+        $(carousel).slick(carouselSettings);
     });
 });
