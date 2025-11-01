84d179b9dffabdb8daac520c14ec481d29defd9f
Sára El-Saig <sara.el-saig@lombiq.com>
2025-11-01 17:02:50 +0100

    Generate version string for Slick resource.

--- Lombiq.UIKit/ResourceManagementOptionsConfiguration.cs
+++ Lombiq.UIKit/ResourceManagementOptionsConfiguration.cs
@@ -1,17 +1,21 @@
+using Lombiq.HelpfulLibraries.Attributes;
 using Lombiq.HelpfulLibraries.OrchardCore.ResourceManagement;
 using static Lombiq.UIKit.Constants.ResourceNames;
 
 namespace Lombiq.UIKit;
 
-public class ResourceManagementOptionsConfiguration : ResourceManagementOptionsConfiguratorBase
+[LibManVersions]
+public partial class ResourceManagementOptionsConfiguration : ResourceManagementOptionsConfiguratorBase
 {
+    private const string Version = LibMan_kenwheeler_slick;
+
     protected override string Area => FeatureIds.Base;
 
     protected override void Configure(ResourceManagementContext context)
     {
-        context.DefineVendorScript(Slick, "slick/slick.min.js", "jQuery");
-        context.DefineVendorStyle(Slick, "slick/slick.css");
-        context.DefineVendorStyle(SlickTheme, "slick/slick-theme.css", Slick);
+        context.DefineVendorScript(Slick, "slick/slick.min.js", "jQuery").SetVersion(Version);
+        context.DefineVendorStyle(Slick, "slick/slick.css").SetVersion(Version);
+        context.DefineVendorStyle(SlickTheme, "slick/slick-theme.css", Slick).SetVersion(Version);
 
         context.DefineScript(LombiqDropdownScript, "dropdown-editor.js");
         context.DefineScript(LombiqTextBoxScript, "textbox-editor.js");
