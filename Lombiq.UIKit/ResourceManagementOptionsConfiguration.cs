a42181698f26b836c2f9c7c093e9e9f1b31f2f33
Sára El-Saig <sara.el-saig@lombiq.com>
2025-11-02 19:41:27 +0100

    Fix slick.

--- Lombiq.UIKit/ResourceManagementOptionsConfiguration.cs
+++ Lombiq.UIKit/ResourceManagementOptionsConfiguration.cs
@@ -9,9 +9,9 @@ public class ResourceManagementOptionsConfiguration : ResourceManagementOptionsC
 
     protected override void Configure(ResourceManagementContext context)
     {
-        context.DefineVendorScript(Slick, "slick/slick.min.js", "jQuery");
-        context.DefineVendorStyle(Slick, "slick/slick.css");
-        context.DefineVendorStyle(SlickTheme, "slick/slick-theme.css", Slick);
+        context.DefineVendorScript(Slick, ("slick/slick.min.js", "slick/slick.js"), "jQuery");
+        context.DefineVendorStyle(Slick, ("slick/slick.min.css", "slick/slick.css"));
+        context.DefineVendorStyle(SlickTheme, ("slick/slick-theme.min.css", "slick/slick-theme.css"), Slick);
 
         context.DefineScript(LombiqDropdownScript, "dropdown-editor.js");
         context.DefineScript(LombiqTextBoxScript, "textbox-editor.js");
